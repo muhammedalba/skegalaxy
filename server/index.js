03c46cd83a8d403cde7a5ec547350835ed24623a
muhammedalbahle <muhammedalbahle@gmail.com>
2024-09-08 01:30:58 +0300

    handle limit

--- server/index.js
+++ server/index.js
@@ -50,7 +50,9 @@ app.post(
   express.raw({ type: "application/json" }),
   webhookCheckout
 );
-app.use(express.json({ limit: "25kb" }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 // app.use(express.json({
 //   verify: (req, res, buf) => {
 //     if (req.originalUrl.startsWith('/stripe/webhook')) {
